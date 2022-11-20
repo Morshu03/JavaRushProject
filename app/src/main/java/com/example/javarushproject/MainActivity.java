9c1f3da134a241a1b9d1d49d2e93feb5b0063baa
Morshu03 <varvaroom03@gmail.com>
2022-11-20 18:35:23 +0300

    Initial

--- app/src/main/java/com/example/javarushproject/MainActivity.java
+++ app/src/main/java/com/example/javarushproject/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.javarushproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
