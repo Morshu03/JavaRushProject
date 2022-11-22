c2b9942aec0cb85683858a3483b94d7558af8716
Morshu03 <varvaroom03@gmail.com>
2022-11-22 19:27:18 +0300

    added first task

--- app/src/main/java/com/example/javarushproject/MainActivity.java
+++ app/src/main/java/com/example/javarushproject/MainActivity.java
@@ -3,22 +3,32 @@ package com.example.javarushproject;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    //TODO: Создать переменную Int = 0
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button submitBtn = findViewById(R.id.btn_order);
+        submitBtn.setOnClickListener(view -> {
+            submitOrder();
+        });
     }
 
     /**
      * This method is called when the order button is clicked.
      */
-    public void submitOrder(View view) {
+    public void submitOrder() {
+        // TODO: Добавлять +2 к переменной
+        //
+        //
         display(1);
     }
 
@@ -26,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given quantity value on the screen.
      */
     private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+        TextView quantityTextView = findViewById(R.id.quantity_text_view);
+        quantityTextView.setText(number);
     }
 }
