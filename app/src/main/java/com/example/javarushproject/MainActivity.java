b2d03c639450f540f8fc8d424c7bfe5a5d9cb501
Morshu03 <varvaroom03@gmail.com>
2022-11-22 16:32:59 +0300

    Initial

--- app/src/main/java/com/example/javarushproject/MainActivity.java
+++ app/src/main/java/com/example/javarushproject/MainActivity.java
@@ -1,18 +1,32 @@
 package com.example.javarushproject;
 
-import android.annotation.SuppressLint;
+
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        display(1);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
     }
 }
