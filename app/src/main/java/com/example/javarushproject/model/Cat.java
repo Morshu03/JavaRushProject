3d98bc611176143de902ce578c07ad0a98a17694
boba <varvaroom03@gmail.com>
2022-12-14 16:18:15 +0300

    fulfilled task with array cats

--- app/src/main/java/com/example/javarushproject/model/Cat.java
+++ app/src/main/java/com/example/javarushproject/model/Cat.java
@@ -1,4 +1,43 @@
 package com.example.javarushproject.model;
 
+import androidx.annotation.NonNull;
+
+import java.util.Objects;
+
 public class Cat {
+    private final int height;
+    private final int weight;
+    private final int lives;
+    private final String name;
+
+
+    public Cat(int height, int weight, int lives, String name) {
+        this.height = height;
+        this.weight = weight;
+        this.lives = lives;
+        this.name = name;
+    }
+
+    @NonNull
+    public String toString(){
+        return "Cat{" +
+                "height=" + height +
+                ", weight=" + weight +
+                ", lives=" + lives +
+                ", name=" + name +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cat cat = (Cat) o;
+        return height == cat.height && weight == cat.weight && lives == cat.lives && Objects.equals(name, cat.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(height, weight, lives, name);
+    }
 }
--- app/src/main/java/com/example/javarushproject/model/Dog.java
+++ app/src/main/java/com/example/javarushproject/model/Dog.java
@@ -1,20 +1,12 @@
 package com.example.javarushproject.model;
 
 import androidx.annotation.NonNull;
-
-import java.util.List;
 import java.util.Objects;
-import java.util.Set;
 
 public class Dog {
-    private int height;
-    private int weight;
-    private String name;
-
-    public Dog() {
-        this.height = 0;
-        this.weight = 0;
-    }
+    private final int height;
+    private final int weight;
+    private final String name;
 
     public Dog(int weight, int height, String name) {
         this.height = height;
@@ -44,58 +36,5 @@ public class Dog {
     public int hashCode() {
         return Objects.hash(height, weight, name);
     }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }
-
-    public int getWeight() {
-        return weight;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName(){
-        return name;
-    }
-
-    //@Override
-    //public boolean equals(Object o) {
-    //    if (this == o) return true;
-    //    if (o == null || getClass() != o.getClass()) return false;
-    //    Dog dog = (Dog) o;
-    //    return getHeight() == dog.getHeight() && getWeight() == dog.getWeight();
-    //}
-
-    //@Override
-    //public int hashCode() {
-    //    return Objects.hash(getHeight(), getWeight());
-    //}
-
-    //public void setHeight(int height) {
-    //    this.height = height;
-    //}
-
-    //public int getHeight() {
-    //    return height;
-    //}
-
-    //public void setWeight(int weight) {
-    //    this.weight = weight;
-    //}
-
-    //public int getWeight() {
-    //    return weight;
-    //}
 
 }
